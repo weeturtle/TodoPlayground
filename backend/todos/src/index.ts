d36251bd1b226136b5661add3eaa64be24137a96
Max London <grahamluk66@gmail.com>
2024-10-17 15:41:52 +0100

    Basic Todo API

--- backend/todos/src/index.ts
+++ backend/todos/src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import bodyParser from "body-parser";
+import todoRouter from "./todos";
 
-const main = () => {
+const main = async () => {
+  const jsonParser = bodyParser.json();
   const app = express();
+  app.use(jsonParser);
   const port = 4001;
 
-  app.get("/", (_, res) => {
-    res.send("App is running");
+  app.get("/", async (_, res) => {
+    res.status(200);
   });
+
+  app.use("/todos", todoRouter);
 
   app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
---
+++ backend/todos/src/todos.ts
@@ -0,0 +1,84 @@
+import { Router } from "express";
+import prisma from "./prisma";
+
+const router = Router();
+
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const todos = await prisma.todo.findMany({
+      where: {
+        userId,
+      },
+    });
+
+    res.status(200).json(todos);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+interface INewTodo {
+  userId: string;
+  title: string;
+}
+
+router.post("/", async (req, res) => {
+  const { userId, title }: INewTodo = req.body;
+
+  try {
+    const todo = await prisma.todo.create({
+      data: {
+        title,
+        userId,
+      },
+    });
+
+    res.status(200).json(todo);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { completed }: { completed: boolean } = req.body;
+
+  try {
+    const todo = await prisma.todo.update({
+      where: {
+        id,
+      },
+      data: {
+        completed,
+      },
+    });
+
+    res.status(200).json(todo);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const todo = await prisma.todo.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.status(200).json(todo);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+export default router;

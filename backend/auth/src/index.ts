fba64efef3299689315770136145ff2841c153ad
Max London <grahamluk66@gmail.com>
2024-10-17 16:52:38 +0100

    Auth service boilerplate

---
+++ backend/auth/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const main = async () => {
+  const jsonParser = bodyParser.json();
+  const app = express();
+  app.use(jsonParser);
+  const port = 4002;
+
+  app.get("/", async (_, res) => {
+    res.status(200).send("Server running");
+  });
+
+  app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+  });
+};
+
+main();
